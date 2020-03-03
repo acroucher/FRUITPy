4689ae381069c1dc449dec242194573c9783babd
Adrian Croucher <a.croucher@auckland.ac.nz>
2020-03-03 14:29:23 +1300

    Add long description in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,17 @@ from __future__ import (absolute_import, division, print_function)
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='FRUITPy',
       version='0.1.0',
       author='Adrian Croucher',
       author_email="a.croucher@auckland.ac.nz",
       description=('Python interface for the FRUIT Fortran unit testing '
                    'framework'),
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/acroucher/FRUITPy',
       classifiers=[
           "Programming Language :: Python :: 2",
